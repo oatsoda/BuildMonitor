e401545f1dd4e9fac94e3a60e033ff2b6f6b9f12
oatsoda <andrew+github@milne.tel>
2015-05-17 18:35:21 +0100

    Add shared AssemblyInfo details. Change version and build setup for 1.0.6

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("TFS Build monitor tray application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrew Milne")]
+[assembly: AssemblyProduct("Build Monitor")]
+[assembly: AssemblyCopyright("Copyright © Andrew Milne 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
