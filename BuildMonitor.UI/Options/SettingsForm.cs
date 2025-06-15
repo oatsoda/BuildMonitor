6ed5af0596eded9259af016af6625c8cc3628d04
oatsoda <andrew+github@milne.tel>
2025-06-15 19:30:32 +0100

    Refactor towards one of the UI patterns

--- BuildMonitor.UI/Options/SettingsForm.cs
+++ BuildMonitor.UI/Options/SettingsForm.cs
@@ -2,6 +2,7 @@
 using BuildMonitor.UI.Helpers;
 using BuildMonitor.UI.Protection;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Security.Authentication;
@@ -12,55 +13,45 @@ namespace BuildMonitor.UI.Options
 {
     internal partial class SettingsForm : Form
     {
+        // Deps
         private readonly IBuildStoreFactory m_BuildStoreFactory;
-        private readonly MonitorOptions m_Options;
 
-        private bool m_SavedSettingsValidated;
+        // Model
+        private readonly ViewModel m_ViewModel;
 
-        public IMonitorOptions Options => m_Options;
+        // Result Property
+        public IMonitorOptions Options => m_ViewModel.Options;
+
+        // Types        
+        private record ADOValidationModel(bool IsValid, string? ErrorMessage, IList<string>? ProjectNames);
+        private class WindowsOptions
+        {
+            public bool RunOnStartup { get; set; }
+        }
+
+        private class ViewModel()
+        {
+            public required MonitorOptions Options { get; init; }
+            public required WindowsOptions Windows { get; init; }
+            public ADOValidationModel? ADOValidationResult { get; set; }
+
+            public bool HasLoadedADOTab => ADOValidationResult != null;
+        }
 
         public SettingsForm(IMonitorOptions currentOptions, IBuildStoreFactory buildStoreFactory)
         {
             InitializeComponent();
             Icon = Properties.Resources._0031_Tools;
 
-            m_Options = new MonitorOptions(currentOptions);
+            m_ViewModel = new ViewModel
+            {
+                Options = new MonitorOptions(currentOptions),
+                Windows = new WindowsOptions { RunOnStartup = StartupSettingHelper.RunOnStartup }
+            };
+
             m_BuildStoreFactory = buildStoreFactory;
 
-            // Windows Tab
-            cbStartup.Checked = StartupSettingHelper.RunOnStartup;
-
-#if DEBUG
-            cbStartup.Enabled = false;
-            cbStartup.Text += " [Option disabled in debug mode]";
-#endif
-
-            // General Tab
-            numInterval.Value = m_Options.IntervalSeconds;
-
-            cbIncludeRunning.Checked = m_Options.IncludeRunningBuilds;
-
-            cbRefreshDefinitions.Checked = m_Options.RefreshDefintions;
-            numDefinitionInterval.Value = m_Options.RefreshDefinitionIntervalSeconds;
-            numDefinitionInterval.Enabled = m_Options.RefreshDefintions;
-
-            cbHideStale.Checked = m_Options.HideStaleDefinitions;
-            numStaleDays.Value = m_Options.StaleDefinitionDays;
-            numStaleDays.Enabled = m_Options.HideStaleDefinitions;
-
-            cbOrderByMostRecent.Checked = m_Options.OrderByMostRecent;
-
-            // ADO Tab
-            txtAdoOrganisation.Text = m_Options.AzureDevOpsOrganisation;
-
-            if (!string.IsNullOrWhiteSpace(m_Options.PersonalAccessTokenCipher))
-            {
-                txtAdoPat.Text = ProtectionMethods.Unprotect(m_Options.PersonalAccessTokenProtected);
-            }
-
-            cboAdoProjectName.SelectedItem = m_Options.ProjectName;
-
-            btnOk.Enabled = cboAdoProjectName.Enabled = m_Options.ValidOptions;
+            UpdateViewFromOptionsModel(m_ViewModel);
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
@@ -70,107 +61,145 @@ namespace BuildMonitor.UI.Options
             if (DialogResult != DialogResult.OK)
                 return;
 
-            // Windows Tab
+            // View-to-Model
+            UpdateOptionsModelFromView(m_ViewModel);
+
+            // Save Model
             StartupSettingHelper.SetStartup(cbStartup.Checked);
-
-            RetrieveOptions(m_Options);
-
-            m_Options.Save();
+            m_ViewModel.Options.Save();
         }
 
-        private void RetrieveOptions(MonitorOptions options)
+        private void UpdateViewFromOptionsModel(ViewModel viewModel)
         {
+            SuspendLayout();
+
+            // Windows Tab
+            cbStartup.Checked = viewModel.Windows.RunOnStartup;
+
+#if DEBUG
+            cbStartup.Enabled = false;
+            cbStartup.Text += " [Option disabled in debug mode]";
+#endif
+
             // General Tab
-            options.IntervalSeconds = (int)numInterval.Value;
+            numInterval.Value = viewModel.Options.IntervalSeconds;
 
-            options.IncludeRunningBuilds = cbIncludeRunning.Checked;
+            cbIncludeRunning.Checked = viewModel.Options.IncludeRunningBuilds;
 
-            options.RefreshDefintions = cbRefreshDefinitions.Checked;
-            options.RefreshDefinitionIntervalSeconds = (int)numDefinitionInterval.Value;
+            cbRefreshDefinitions.Checked = viewModel.Options.RefreshDefintions;
+            numDefinitionInterval.Value = viewModel.Options.RefreshDefinitionIntervalSeconds;
+            numDefinitionInterval.Enabled = viewModel.Options.RefreshDefintions;
 
-            options.HideStaleDefinitions = cbHideStale.Checked;
-            options.StaleDefinitionDays = (int)numStaleDays.Value;
+            cbHideStale.Checked = viewModel.Options.HideStaleDefinitions;
+            numStaleDays.Value = viewModel.Options.StaleDefinitionDays;
+            numStaleDays.Enabled = viewModel.Options.HideStaleDefinitions;
 
-            options.OrderByMostRecent = cbOrderByMostRecent.Checked;
+            cbOrderByMostRecent.Checked = viewModel.Options.OrderByMostRecent;
 
             // ADO Tab
-            options.AzureDevOpsOrganisation = txtAdoOrganisation.Text;
-            options.PersonalAccessTokenProtected = ProtectionMethods.Protect(txtAdoPat.Text);
+            txtAdoOrganisation.Text = viewModel.Options.AzureDevOpsOrganisation;
 
-            if (m_SavedSettingsValidated) // Tab never loaded, so don't overwrite this setting because it will not be loaded
-                options.ProjectName = (string)cboAdoProjectName.SelectedItem!;
+            if (!string.IsNullOrWhiteSpace(viewModel.Options.PersonalAccessTokenCipher))
+            {
+                txtAdoPat.Text = ProtectionMethods.Unprotect(viewModel.Options.PersonalAccessTokenProtected);
+            }
 
-            options.ValidOptions = cboAdoProjectName.Enabled;
+            cboAdoProjectName.SelectedItem = viewModel.Options.ProjectName;
+
+            btnOk.Enabled = viewModel.Options.ValidOptions;
+
+            ResumeLayout();
         }
 
-        private async void btnValidate_Click(object sender, EventArgs e)
+        private void UpdateADOViewFromModel(ViewModel viewModel)
         {
-            await ValidateADOSettings();
+            SuspendLayout();
+
+            // There's two kinds of validation going on here: ADO creds, and whether we can save settings (i.e. > 1 Project returned)
+
+            cboAdoProjectName.Enabled =
+                btnOk.Enabled = viewModel.ADOValidationResult!.IsValid && viewModel.ADOValidationResult.ProjectNames!.Count > 0;
+
+            cboAdoProjectName.Items.AddRange([.. viewModel.ADOValidationResult.ProjectNames!.Cast<object>()]);
+
+            cboAdoProjectName.SelectedItem = viewModel.Options.ProjectName ?? viewModel.ADOValidationResult.ProjectNames!.FirstOrDefault();
+
+            imgBox.Image = viewModel.ADOValidationResult.IsValid
+                ? Status.Succeeded.ToBitmap(new Size(24, 24))
+                : Status.Failed.ToBitmap(new Size(24, 24));
+
+            if (viewModel.ADOValidationResult.ErrorMessage != null)
+            {
+                MessageBox.Show(viewModel.ADOValidationResult.ErrorMessage, @"Failed to fetch ADO Projects", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            ResumeLayout();
         }
 
-        private async void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        private void UpdateOptionsModelFromView(ViewModel viewModel)
         {
-            if (tabControl.SelectedTab != tabADO)
-                return;
+            // Windows Tab
+            viewModel.Windows.RunOnStartup = cbStartup.Checked;
 
-            if (m_SavedSettingsValidated)
-                return;
+            // General Tab
+            viewModel.Options.IntervalSeconds = (int)numInterval.Value;
 
-            await ValidateADOSettings();
+            viewModel.Options.IncludeRunningBuilds = cbIncludeRunning.Checked;
+
+            viewModel.Options.RefreshDefintions = cbRefreshDefinitions.Checked;
+            viewModel.Options.RefreshDefinitionIntervalSeconds = (int)numDefinitionInterval.Value;
+
+            viewModel.Options.HideStaleDefinitions = cbHideStale.Checked;
+            viewModel.Options.StaleDefinitionDays = (int)numStaleDays.Value;
+
+            viewModel.Options.OrderByMostRecent = cbOrderByMostRecent.Checked;
+
+            // ADO Tab
+            viewModel.Options.AzureDevOpsOrganisation = txtAdoOrganisation.Text;
+            viewModel.Options.PersonalAccessTokenProtected = ProtectionMethods.Protect(txtAdoPat.Text);
+
+            if (viewModel.HasLoadedADOTab) // Tab never loaded, so don't overwrite this setting because it will not be loaded
+                viewModel.Options.ProjectName = (string)cboAdoProjectName.SelectedItem!;
+
+            viewModel.Options.ValidOptions = cboAdoProjectName.Enabled;
         }
 
-        private async Task ValidateADOSettings()
+        private async Task RunADOValidation()
         {
             tabADO.Enabled = false;
 
-            var tempOptions = new MonitorOptions();
-            RetrieveOptions(tempOptions);
+            m_ViewModel.ADOValidationResult = await ValidateADOSettings();
+            UpdateADOViewFromModel(m_ViewModel);
 
-            if (string.IsNullOrWhiteSpace(tempOptions.AzureDevOpsOrganisation))
-            {
-                cboAdoProjectName.Enabled = false;
-                btnOk.Enabled = false;
-                tabADO.Enabled = true;
-                return;
-            }
+            tabADO.Enabled = true;
+        }
 
-            cboAdoProjectName.Items.Clear();
+        private async Task<ADOValidationModel> ValidateADOSettings()
+        {
+            var tempViewModel = new ViewModel { Options = new(), Windows = new() };
+            UpdateOptionsModelFromView(tempViewModel);
+
             try
             {
-                var store = m_BuildStoreFactory.GetBuildStore(tempOptions, true);
+                var store = m_BuildStoreFactory.GetBuildStore(tempViewModel.Options, true);
                 var projects = await store.GetProjects();
-                cboAdoProjectName.Items.AddRange(projects.Cast<object>().ToArray());
-                imgBox.Image = Status.Succeeded.ToBitmap(new Size(24, 24));
+                return new ADOValidationModel(true, null, [.. projects]);
             }
             // filters handle Aggregation Exception
             catch (Exception ex) when (ex is AuthenticationException ||
                     ex.GetBaseException() is AuthenticationException)
             {
-                imgBox.Image = Status.Failed.ToBitmap(new Size(24, 24));
+                return new ADOValidationModel(false, null, null);
             }
             catch (Exception ex)
             {
-                imgBox.Image = Status.Failed.ToBitmap(new Size(24, 24));
-                MessageBox.Show(ex.ToString());
+                return new ADOValidationModel(false, ex.ToString(), null);
             }
-
-            if (cboAdoProjectName.Items.Count > 0)
-            {
-                m_SavedSettingsValidated = true;
-                cboAdoProjectName.SelectedItem = !string.IsNullOrWhiteSpace(m_Options.ProjectName)
-                    ? m_Options.ProjectName
-                    : cboAdoProjectName.Items[0];
-
-                cboAdoProjectName.Enabled = true;
-                btnOk.Enabled = true;
-                tabADO.Enabled = true;
-                return;
-            }
-
-            cboAdoProjectName.Enabled = false;
-            btnOk.Enabled = false;
-            tabADO.Enabled = true;
         }
+
+        #region Event Handlers
+
+        // ** General Tab
 
         private void cbRefreshDefinitions_CheckedChanged(object sender, EventArgs e)
         {
@@ -181,6 +210,40 @@ namespace BuildMonitor.UI.Options
         {
             numStaleDays.Enabled = cbHideStale.Checked;
         }
+
+        // ** ADO Tab
+
+        private void txtAdoOrganisation_TextChanged(object sender, EventArgs e)
+        {
+            lblLinkPat.Enabled =
+                btnValidate.Enabled = txtAdoOrganisation.Text.Length >= 0;
+
+            var url = $"https://dev.azure.com/{txtAdoOrganisation.Text}/_usersSettings/tokens";
+            lblLinkPat.SetUrl(url);
+        }
+
+        private void lblLinkPat_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.VisitUrl(sender);
+        }
+
+        private async void btnValidate_Click(object sender, EventArgs e)
+        {
+            await RunADOValidation();
+        }
+
+        private async void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl.SelectedTab != tabADO)
+                return;
+
+            if (m_ViewModel.HasLoadedADOTab)
+                return;
+
+            await RunADOValidation();
+        }
+
+        // ** Other Events
 
         private void btnReset_Click(object sender, EventArgs e)
         {
@@ -193,22 +256,10 @@ namespace BuildMonitor.UI.Options
                 ))
                 return;
 
-            m_Options.Reset();
+            m_ViewModel.Options.Reset();
             DialogResult = DialogResult.Abort; // Close without triggering OnClose saving of settings.
         }
 
-        private void txtAdoOrganisation_TextChanged(object sender, EventArgs e)
-        {
-            lblLinkPat.Enabled = txtAdoOrganisation.Text.Length >= 0;
-
-            var url = $"https://dev.azure.com/{txtAdoOrganisation.Text}/_usersSettings/tokens";
-            lblLinkPat.SetUrl(url);
-        }
-
-        private void lblLinkPat_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            e.VisitUrl(sender);
-        }
-
+        #endregion
     }
 }
